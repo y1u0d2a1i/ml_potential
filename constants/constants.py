4e52c2cddee95332099b4161c369897f35f9d637
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-08-21 20:59:45 +0900

    add: get n_atom and n_data_instance

--- constants/constants.py
+++ constants/constants.py
@@ -1,9 +1,12 @@
 from glob import glob
+import pandas as pd
 from ml_utils.util import flatten
+from scf.get_lattice_info import QELattice
 
 
 def get_path2data():
     return '/Users/y1u0d2/desktop/Lab/data/qe_data/Si'
+
 
 def get_silicon_all_scf_data():
     path2data = get_path2data()
@@ -15,3 +18,16 @@ def get_silicon_all_scf_data():
         all_path.append(structures)
     all_path = list(flatten(all_path))
     return all_path
+
+
+def get_all_structure_info():
+    all_dirs = get_silicon_all_scf_data()
+    result = []
+    for path in all_dirs:
+        qel = QELattice(path)
+        mpid = list(filter(lambda x: 'mp-' in x, path.split('/')))[0]
+        n_atom = qel.num_atom
+        result.append([mpid, n_atom])
+    df = pd.DataFrame(data=result, columns=['mpid', 'n_atom'])
+    pt = pd.pivot_table(df, index=['mpid', 'n_atom'], aggfunc=len, margins=True)
+    return pt
