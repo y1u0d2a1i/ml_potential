60da6d0a51bd618da6e2e4be524b739609a56814
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-09-07 22:32:00 +0900

    changed print log

--- create_input/get_filtered_structure_data.py
+++ create_input/get_filtered_structure_data.py
@@ -1,8 +1,5 @@
-import glob
-import os
+import collections
 from constants.constants import get_silicon_all_scf_data
-from create_input.from_scf import N2p2ScfParser
-from scf.scf_util import flatten
 from create_input.from_scf_minus_lmp import ScfMinusLammpsN2p2
 
 
@@ -10,6 +7,7 @@ def get_filtered_structure_path(energy_threshold: float) -> list[str]:
     all_dirs = get_silicon_all_scf_data()
 
     filtered_data = []
+    removed_data = []
     for d in all_dirs:
         try:
             n2p2_obj = ScfMinusLammpsN2p2(
@@ -19,9 +17,15 @@ def get_filtered_structure_path(energy_threshold: float) -> list[str]:
             )
             if n2p2_obj.get_calced_energy() / n2p2_obj.num_atom > energy_threshold:
                 filtered_data.append(d)
+            else:
+                structure_id = list(filter(lambda x: 'mp-' in x, d.split('/')))[0]
+                removed_data.append(structure_id)
         except:
             continue
     
     num_removed_data = len(all_dirs) - len(filtered_data)
+
+    collection = collections.Counter(removed_data)
+    print(dict(collection))
     print(f'{num_removed_data} structures were removed')
     return filtered_data
