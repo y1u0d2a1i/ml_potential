5360a0069e93c501bf6bd757a366f25196a26173
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-10-06 21:10:53 +0900

    add is_zbl flag

--- create_input/get_filtered_structure_data.py
+++ create_input/get_filtered_structure_data.py
@@ -2,6 +2,8 @@ import numpy as np
 import collections
 from constants.constants import get_silicon_all_scf_data
 from create_input.from_scf_minus_lmp import ScfMinusLammpsN2p2
+from create_input.from_scf import N2p2ScfParser
+from scf.qelattice import get_qel
 
 
 def get_filtered_structure_path_by_energy(energy_threshold: float) -> list[str]:
@@ -32,20 +34,25 @@ def get_filtered_structure_path_by_energy(energy_threshold: float) -> list[str]:
     return filtered_data
 
 
-def get_filtered_structure_path_by_force(force_threshold: float) -> list[str]:
+def get_filtered_structure_path_by_force(force_threshold: float, is_zbl=True) -> list[str]:
     all_dirs = get_silicon_all_scf_data()
 
     filtered_data = []
     removed_data = []
     for d in all_dirs:
         try:
-            n2p2_obj = ScfMinusLammpsN2p2(
-                path2scfout=d,
-                dump_filename='dump.out',
-                is_comment=True
-            )
+            if is_zbl:
+                n2p2_obj = ScfMinusLammpsN2p2(
+                    path2scfout=d,
+                    dump_filename='dump.out',
+                    is_comment=True
+                )
+                force = n2p2_obj.get_calced_force()
+            else:
+                qel = get_qel(d)
+                force = qel.get_force()
 
-            if np.all(np.abs(n2p2_obj.get_calced_force()) < force_threshold):
+            if np.all(np.abs(force) < force_threshold):
                 filtered_data.append(d)
             else:
                 structure_id = list(filter(lambda x: 'mp-' in x, d.split('/')))[0]
